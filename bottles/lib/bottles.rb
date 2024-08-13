01b01f18d2c19d01a788c1941bb832479f323acc
Curtis Kelsey <curtiskelsey@axiscare.com>
2024-08-13 17:03:54 -0500

    Shameless green first two tests

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,3 +1,8 @@
 class Bottles
-
+    def verse(n)
+        return  (n).to_s + " bottles of beer on the wall, " +
+            (n).to_s + " bottles of beer.\n" +
+            "Take one down and pass it around, " +
+            (n - 1).to_s + " bottles of beer on the wall.\n"
+    end
 end
